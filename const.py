ec9060a57cc5d1106a692dea95747860f3fba809
Domusteck.it <info@domusteck.it>
2026-04-25 19:48:27 +0200

    Delete const.py

--- const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "carburanti_italia"
-
-# Campi configurazione
-CONF_CITY = "city"
-CONF_PROVINCE = "province"
-CONF_FUEL_TYPE = "fuel_type"
-
-# Intervallo aggiornamento (in ore)
-UPDATE_INTERVAL_HOURS = 1
